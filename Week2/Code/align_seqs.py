78d59c6602c2d9729052acced928680c9a478b24
Luke Swaby <lds20@ic.ac.uk>
2020-10-22 20:42:41 +0100

    Now iterates from first overlap of the two seqs

--- Week2/Code/align_seqs.py
+++ Week2/Code/align_seqs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#TODO: Write README.md
+# TODO: Write README.md
 
 """Aligns 2 sequences in an input text file."""
 
@@ -8,33 +8,34 @@ __author__ = 'Luke Swaby (lds20@ic.ac.uk)'
 __version__ = '0.0.1'
 
 ## Imports ##
-import sys
+import sys, re
 
 ## Functions ##
-def calculate_score(s1, s2, l1, l2, startpoint):
-    """Calculate best alignment score (no. of matched bases) of 2
-    sequences
+def calculate_score(s1, s2, startpoint):
+    """Calculate best alignment score (no. of matched bases) of 2 padded
+     sequences
     """
-    matched = "" # to hold string displaying alignements
+    matched = ""  # to hold string displaying alignements
     score = 0
-    for i in range(l2):
-        if (i + startpoint) < l1:
-            if s1[i + startpoint] == s2[i]:
-                # if the bases match, fill with '*' and add 1 to score
+    for i in range(len(s1)):
+        if (i + startpoint) < len(s1):
+            if s1[i + startpoint] == s2[i] and s2[i] != '-':
+                # If match, add '*' to matched str and increment score
                 matched += "*"
                 score += 1
             else:
-                # otherwise fill with '-'
+                # otherwise add '-' to matched str
                 matched += "-"
-    # import ipdb; ipdb.set_trace()
 
     # some formatted output
-    matches = "." * startpoint + matched
+    matches = "-" * startpoint + matched
     print(matches)
-    print("." * startpoint + s2)
+    print("-" * startpoint + s2[:len(s2)-startpoint])
+    # Note: cannot use 'print("-" * startpoint + s2[:-startpoint])' here^ as
+    # negative indexing won't work for i=0
     print(s1)
-    print(score)
-    print(" ")
+    print(f'Score: {score}')
+    print('\n')
 
     return score, matches
 
@@ -49,79 +50,63 @@ def main(argv):
     else:
         sys.exit('ERROR: too many arguments were provided.')
 
-    ## TEXT IN
+    # Text file in
     with open(seqfile, 'r') as f:
         recs = f.read().splitlines()  # To strip newlines
         seq1 = recs[0]
         seq2 = recs[1]
 
-    """
-    ## FASTA IN
-    from Bio import SeqIO
-    try:
-        seqfile = argv[1]
-    except:
-        seqfile = '../Data/seqs.fa'
-
-    recs = list(SeqIO.parse(seqfile, 'fasta'))
-    seq1 = str(recs[0].seq)
-    seq2 = str(recs[1].seq)
-    """
-
     # Assign the longer sequence s1, and the shorter to s2
     # l1 is length of the longest, l2 that of the shortest
-
     l1 = len(seq1)
     l2 = len(seq2)
     if l1 >= l2:
-        s1 = seq1
-        s2 = seq2
+        s1algn = (l2-1)*'-' + seq1 + (l2-1)*'-'
+        s2algn = seq2 + (l1+l2-2)*'-'
+        padlen = len(s2algn)
     else:
-        s1 = seq2
-        s2 = seq1
         l1, l2 = l2, l1  # swap the two lengths
+        s1algn = (l2-1)*'-' + seq2 + (l2-1)*'-'
+        s2algn = seq1 + (l1+l2-2)*'-'
+        padlen = len(s2algn)
 
-    # now try to find the best match (highest score) for the two sequences
+    # Find best match (highest score) for the two sequences
     my_best_align = None
     my_best_score = -1
 
-    for i in range(
-            l1):  # Note that you just take the last alignment with the highest score
-        z, match = calculate_score(s1, s2, l1, l2, i)
+    for i in range(padlen-l2+1):
+        # Note that only last alignment with the highest score is recorded
+        print(f'Startpoint: {i}')
+        z, match = calculate_score(s1algn, s2algn, i)
         if z > my_best_score:
-            my_best_align = ("-" * i) + s2  # start sequence at startpoint
+            my_best_align = "-"*i + s2algn[:-i]  # start sequence at startpoint
             my_best_score = z
             my_best_match = match
-    print(my_best_match.replace('-', '.'))
+
+    # Clip trailing hyphens
+    s1start = re.search(r'[^-]', s1algn).start()
+    s2start = re.search(r'[^-]', my_best_align).start()
+    start = max(s1start, s2start)
+
+    s1end = re.search(r'[^A-Z]', s1algn[start:]).start()
+    s2end = re.search(r'[^A-Z]', my_best_align[start:]).start()
+    stop = len(s1algn[start:]) - max(s1end, s2end)
+
+    my_best_match = my_best_match[start:-stop]
+    my_best_align = my_best_align[start:-stop]
+    s1algn = s1algn[start:-stop]
+
+    # Print result
+    print(my_best_match)
     print(my_best_align)
-    print(s1)
+    print(s1algn)
     print("Best score:", my_best_score)
 
-    """
-    ## FASTA OUT
-    from Bio import SeqIO
-    from Bio.SeqRecord import SeqRecord
-    from Bio.Seq import Seq
-    
-    outrec1 = SeqRecord(
-        Seq(my_best_align),
-        id="Seq1",
-        description=f'Best Score: {my_best_score}'
-    )
-    outrec2 = SeqRecord(
-        Seq(s1),
-        id="Seq2",
-        description=''
-    )
-    outrecs = [outrec1, outrec2]
-    SeqIO.write(outrecs, '../Results/algmt.fa', 'fasta')
-    """
-
-    ## TEXT OUT
+    # Text file out
     with open(f'../Results/algmt.txt', 'w') as algmt:
-        algmt.write(f"{my_best_match.replace('-', '.')}\n")
+        algmt.write(f"{my_best_match}\n")
         algmt.write(f'{my_best_align}\n')
-        algmt.write(str(s1))
+        algmt.write(str(s1algn))
         algmt.write('\n\n' + f"Best score: {my_best_score}")
 
     return 0
